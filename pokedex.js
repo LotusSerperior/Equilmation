ddd6bf0373c9b6ed4de4f23d2b6ba90ab4f7b59d
LotusSerperior <roboticjo123@gmail.com>
2016-12-16 11:52:44 +1100

    Create pokedex.js

---
+++ pokedex.js
@@ -0,0 +1,71 @@
+'use strict';
+
+exports.BattlePokedex = {
+  	equilmation: {
+	      	num: 10091,
+	      	species: "Equilmation",
+	      	types: ["Normal"],
+		      baseStats: {hp: 90, atk: 90, def: 90, spa: 90, spd: 90, spe: 90},
+	      	abilities: {0: "Clear Body"},
+	      	heightm: 1.9,
+	      	weightkg: 42,
+	      	color: "White"
+	      	eggGroups: ["Amorphous"],
+          otherFormes: ["Equilmation-Combat", "Equilmation-Telepath", "Equilmation-Shadow", "Equilmation-Tank", "Equilmation-Nimble"],
+     },
+     equilmationcombat: {
+	      	num: 10091,
+	      	species: "Equilmation-Combat",
+	      	types: ["Fighting", "Dark"],
+		      baseStats: {hp: 90, atk: 140, def: 80, spa: 40, spd: 50, spe: 130},
+	      	abilities: {0: "Reliance"},
+	      	heightm: 2.2,
+	      	weightkg: 90,
+	      	color: "White"
+	      	eggGroups: ["Amorphous"],
+     },
+     equilmationtelepath: {
+          num: 10091,
+	      	species: "Equilmation-Telepath",
+	      	types: ["Psychic", "Grass"],
+		      baseStats: {hp: 90, atk: 40, def: 80, spa: 140, spd: 130, spe: 50},
+	      	abilities: {0: "Psychic Barrier"},
+	      	heightm: 1.9,
+	      	weightkg: 30,
+	      	color: "White"
+	      	eggGroups: ["Amorphous"],
+     },
+     equilmationshadow: {
+          num: 10091,
+	      	species: "Equilmation-Shadow",
+	      	types: ["Ghost", "Normal"],
+		      baseStats: {hp: 90, atk: 90, def: 50, spa: 90, spd: 80, spe:130},
+	      	abilities: {0: "First Strike"},
+	      	heightm: 1.9,
+	      	weightkg: 0.3,
+	      	color: "White"
+	      	eggGroups: ["Amorphous"],
+     },
+     equilmationtank: {
+          num: 10091,
+	      	species: "Equilmation-Tank",
+	      	types: ["Steel", "Ground"],
+		      baseStats: {hp: 90, atk: 80, def: 140, spa: 40, spd: 130, spe: 50},
+	      	abilities: {0: "Sentry"},
+	      	heightm: 3.2,
+	      	weightkg: 210,
+	      	color: "White"
+	      	eggGroups: ["Amorphous"],
+     },
+     equilmationnimble: {
+          num: 10091,
+	      	species: "Equilmation-Nimble",
+	      	types: ["Fire", "Fairy"],
+		      baseStats: {hp: 90, atk: 130, def: 20, spa: 130, spd: 20, spe: 150},
+	      	abilities: {0: "Advantageous"},
+	      	heightm: 1.1,
+	      	weightkg: 0.1,
+	      	color: "White"
+	      	eggGroups: ["Amorphous"],
+     },
+};
